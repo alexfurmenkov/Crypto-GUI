8b2057174c82d527eb05643f03e00264ff504e46
alexfurmenkov <alexfurm@mail.ru>
2020-05-19 00:31:42 +0200

    [UPDATE] Write keys to a file

--- src/crypto/crypto.py
+++ src/crypto/crypto.py
@@ -10,4 +10,10 @@ class Crypto:
         self.public_key = public_key
         self.private_key = private_key
 
+        with open('public_key.pem', 'wb') as f:
+            f.write(rsa.PublicKey.save_pkcs1(self.public_key, format='PEM'))
+
+        with open('private_key.pem', 'wb') as f:
+            f.write(rsa.PrivateKey.save_pkcs1(self.private_key, format='PEM'))
+
         return public_key, private_key
