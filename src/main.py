a5f3e3c63373f3d1d1ac3ae5a33cdb20bb7ba255
alexfurmenkov <alexfurm@mail.ru>
2020-05-19 00:32:43 +0200

    [UPDATE] Implement file encryption and decryption

--- src/main.py
+++ src/main.py
@@ -4,16 +4,19 @@ from tkinter import (
     Label,
     Entry,
     Button,
-    Text,
     CENTER,
-    DISABLED,
-    messagebox
+    messagebox,
+    filedialog
 )
+
+import rsa
 
 from sqlalchemy.orm import sessionmaker
 
 from src.db import User, engine
 from src.crypto import Crypto
+
+session = sessionmaker(bind=engine)()
 
 main_window = Tk()
 main_window.geometry('800x600')
@@ -25,8 +28,8 @@ class Actions:
         self.master = master
         self.user = user
         self.button_generate = Button(self.master, text='Сгенерировать ключи', command=self.generate_keys)
-        self.button_encrypt = Button(self.master, text='Зашифровать файл')
-        self.button_decrypt = Button(self.master, text='Расшифровать файл')
+        self.button_encrypt = Button(self.master, text='Зашифровать файл', command=self.encrypt_file)
+        self.button_decrypt = Button(self.master, text='Расшифровать файл', command=self.decrypt_file)
 
         self.master.geometry('800x600')
         self.button_generate.pack(pady=10)
@@ -37,17 +40,35 @@ class Actions:
         crypto = Crypto()
         crypto.generate_keys()
 
-        public_text = Text(self.master, height=1, relief='flat')
-        private_text = Text(self.master, height=1, relief='flat')
+        messagebox.showinfo(title='Успех', message='Пара ключей была сгенерирована.')
 
-        public_text.insert(1.0, crypto.public_key)
-        private_text.insert(1.0, crypto.private_key)
-        public_text.config(state=DISABLED)
-        private_text.config(state=DISABLED)
-        public_text.place(relx=0.5, rely=0.48, anchor=CENTER)
-        private_text.place(relx=0.5, rely=0.58, anchor=CENTER)
+        data = dict(
+            public_key='public_key.pem',
+            private_key='private_key.pem',
+        )
+        session.query(User).filter_by(id=self.user.id).update(data)
+        session.commit()
 
-        #  добавить сюда присваивание этих ключей объекту класса User
+    def encrypt_file(self):
+        filename = filedialog.askopenfilename()
+        if filename is not None:
+            with open(filename, 'rb+') as file:
+                content = file.read()
+                file.seek(0)
+                content = rsa.encrypt(content, self.user.get_public_key())
+                file.write(content)
+                messagebox.showinfo(title='Успех', message='Содержимое файла зашифровано.')
+
+    def decrypt_file(self):
+        filename = filedialog.askopenfilename()
+        if filename is not None:
+            with open(filename, 'rb+') as file:
+                content = file.read()
+                content = rsa.decrypt(content, self.user.get_private_key())
+                file.truncate(0)
+                file.seek(0)
+                file.write(content)
+                messagebox.showinfo(title='Успех', message='Содержимое файла расшифровано.')
 
 
 class Login:
@@ -63,7 +84,6 @@ class Login:
 
     def login_user(self):
         user_login = self.login_input.get()
-        session = sessionmaker(bind=engine)()
         user = session.query(User).filter_by(login=user_login).first()
         if user is not None:
             Actions(Toplevel(self.master), user)
